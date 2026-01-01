54858a48924310e2467f896e9352ae80bb1b8754
Ivan Gospodinov <ivan9069@gmail.com>
2026-01-01 18:03:14 +0200

    change file 01_reverse_numbers

--- 01_02_lists_as_stacks_and_queues - Exercise/01_reverse_numbers.py
+++ 01_02_lists_as_stacks_and_queues - Exercise/01_reverse_numbers.py
@@ -1,7 +1,10 @@
 numbers = input().split()
 
-reversed_numbers = []
-for number in range(len(numbers)):
-    last_number = numbers.pop()
-    reversed_numbers.append(last_number)
-print(" ".join(reversed_numbers))
+# reversed_numbers = []
+# for number in range(len(numbers)):
+#     last_number = numbers.pop()
+#     reversed_numbers.append(last_number)
+# print(" ".join(reversed_numbers))
+
+while numbers:
+    print(f"{numbers.pop()}", end=' ')
