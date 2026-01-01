ce7bfb87e5e96777f8796b162d4754267581f20f
Ivan Gospodinov <ivan9069@gmail.com>
2026-01-01 18:18:50 +0200

    add file 02_stacked_queries.py

--- 01_02_lists_as_stacks_and_queues - Exercise/02_stacked_queries.py
+++ 01_02_lists_as_stacks_and_queues - Exercise/02_stacked_queries.py
@@ -5,7 +5,7 @@ function = {
     "1": lambda x: my_stack.append(x),
     "2": lambda: my_stack.pop() if my_stack else None,
     "3": lambda: max(my_stack) if my_stack else None,
-    "4": lambda: min(my_stack)if my_stack else None,
+    "4": lambda: min(my_stack) if my_stack else None,
 }
 
 for _ in range(n):
