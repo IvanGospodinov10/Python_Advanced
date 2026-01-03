ec72233c1cb5f185eb99ab471229aa0ee0973d70
Ivan Gospodinov <ivan9069@gmail.com>
2026-01-03 23:48:20 +0200

    add file 10_cups_and_bottles.py

---
+++ 01_02_lists_as_stacks_and_queues - Exercise/10_cups_and_bottles.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+cups = deque(int(x) for x in input().split())
+bottles = [int(x) for x in input().split()]
+
+wasted_water = 0
+
+while cups and bottles:
+    cup = cups[0]
+    bottle = bottles[-1]
+    while cup > 0:
+        if cup > bottle:
+            cup = cup - bottle
+            bottles.pop()
+            bottle = bottles[-1]
+        else:
+            wasted_water += bottle - cup
+            cup = cup - bottle
+            bottles.pop()
+            cups.popleft()
+
+if cups and not bottles:
+    print(f'Cups: {" ".join(map(str, cups))}')
+    print(f"Wasted litters of water: {wasted_water}")
+else:
+    while bottles:
+        print(f"{bottles.pop()}", end=' ')
+        print(f"\nWasted litters of water: {wasted_water}")
