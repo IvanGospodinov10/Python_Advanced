da862485fd42d2642f65f24b9546b1c71dad4504
Ivan Gospodinov <ivan9069@gmail.com>
2026-01-09 18:07:34 +0200

    change file 01_02_lists_as_stacks_and_queues - Exercise/02_stacked_queries.py

--- 01_02_lists_as_stacks_and_queues - Exercise/02_stacked_queries.py
+++ 01_02_lists_as_stacks_and_queues - Exercise/02_stacked_queries.py
@@ -2,7 +2,7 @@ n = int(input())
 my_stack = []
 
 function = {
-    "1": lambda x: my_stack.append(x),
+    "1": lambda x: my_stack.append(int(x)),
     "2": lambda: my_stack.pop() if my_stack else None,
     "3": lambda: print(max(my_stack)) if my_stack else None,
     "4": lambda: print(min(my_stack)) if my_stack else None
@@ -11,4 +11,4 @@ function = {
 for _ in range(n):
     query = input().split()
     function[query[0]](*query[1:])
-print(" ".join([str(x) for x in reversed(my_stack)]))
+print(", ".join([str(x) for x in reversed(my_stack)]))
